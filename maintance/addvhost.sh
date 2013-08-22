b5db7602a0ec1a103c72b4f4ef7aa140149db41c
zhongwei.bzw <zhongwei.bzw@taobao.com>
2013-08-22 12:18:38 +0800

    addvhosts完成 nginx's uid 待写

--- maintance/addvhost.sh
+++ maintance/addvhost.sh
@@ -25,8 +25,9 @@ user_pwd=$(md5sum </proc/uptime | cut -d " " -f 1)
 user_pwd=${user_pwd:0:5}
 useradd -m $user_name -p $(mkpasswd $user_pwd)
 mkdir -p /home/$user_name/logs
+mkdir -p /home/$user_name/tmp
 chown $user_name:$user_name /home/$user_name/ -R
-chmod 750 /home/$user_name -R
+chmod 755 /home/$user_name -R
 echo "your password is $user_pwd"
 
 echo "+--------------------------------------+"
@@ -34,11 +35,14 @@ echo "+              adding vhost            +"
 echo "+--------------------------------------+"
 mkdir -p /home/$user_name/$domain_name
 # init fpm poll
-
+cp configs/fpm-poll.conf.default ${etc_dir}fpm.d/$domain_name.conf
+sed -i -e "s/{{user_name}}/${user_name}/g" ${etc_dir}fpm.d/$domain_name.conf
+sed -i -e "s/{{domain_name}}/${domain_name}/g" ${etc_dir}fpm.d/$domain_name.conf
 # init nginx port
-cat >> ${etc_dir}nginx.d/$domain_name.conf << EOF
-EOF
-
+cp configs/nginx.vhosts.conf.default ${etc_dir}nginx.d/$domain_name.conf
+sed -i -e "s/{{user_name}}/${user_name}/g" ${etc_dir}nginx.d/$domain_name.conf
+sed -i -e "s/{{domain_name}}/${domain_name}/g" ${etc_dir}nginx.d/$domain_name.conf
+./restart.sh
 echo "+--------------------------------------+"
 echo "+              finish                  +"
 echo "+--------------------------------------+"
